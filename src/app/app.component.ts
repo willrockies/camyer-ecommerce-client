51ee23d8e3a1a7e299e6c907c081fff907195aff
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-07-26 08:44:36 -0300

    84.creating a products interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Camyer';
-  products: any[] = []
+  products: Product[] = []
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
       next: (response: any) => this.products = response.data,
       error: error => console.log(error),
       complete: () => {
