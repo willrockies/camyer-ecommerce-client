59418edecabc261dad9d6441ed384525a87abc46
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-07-26 11:22:30 -0300

    93.Adding the products filters

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ShopService } from './shop.service';
 import { Product } from '../shared/models/product';
+import { Type } from '../shared/models/type';
 
 @Component({
   selector: 'app-shop',
@@ -9,10 +10,16 @@ import { Product } from '../shared/models/product';
 })
 export class ShopComponent implements OnInit {
   products: Product[] = [];
+  types: Type[] = [];
 
   constructor(private shopService: ShopService) {}
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getTypes();
+  }
+
+  getProducts(){
     this.shopService.getProducts().subscribe({
       next: response => this.products = response.data,
       error: error => console.log(error),
@@ -20,5 +27,11 @@ export class ShopComponent implements OnInit {
     })
   }
 
-
+  getTypes(){
+    this.shopService.getTypes().subscribe({
+      next: response => this.types = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
 }
--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Pagination } from '../shared/models/pagination';
 import { Product } from '../shared/models/product';
+import { Type } from '../shared/models/type';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class ShopService {
 
   getProducts() {
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products?pageSize=50');
+  }
+
+  getTypes(){
+    return this.http.get<Type[]>(this.baseUrl + 'products/types');
   }
 }
