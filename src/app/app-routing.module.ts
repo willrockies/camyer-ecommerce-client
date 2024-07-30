1c238a17b75a7321ab8e806f7b4bd8ec5a1c1e4d
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-07-30 14:43:49 -0300

    116.Creating an error component for testing the error responses

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'shop', loadChildren: () => import ('./shop/shop.module').then(mod => mod.ShopModule)},
-  {path: '**', redirectTo: '', pathMatch: 'full'},
+  { path: '', component: HomeComponent },
+  { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule) },
+  { path: 'test-error', component: TestErrorComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
