b3fc513d47b2348fbd084e3d044a6f3d1f51da6e
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-01 09:07:04 -0300

    Adding a section header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,16 +6,19 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 @NgModule({
   declarations: [
     NavBarComponent,
     TestErrorComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    SectionHeaderComponent
   ],
   exports: [
-    NavBarComponent
+    NavBarComponent,
+    SectionHeaderComponent,
   ],
   imports: [
     CommonModule,
