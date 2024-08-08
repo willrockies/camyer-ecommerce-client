69ad08e272cf8eb1a8697dcc6f276389c8d435b1
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-08 11:35:16 -0300

    153.Styling the basket page

--- src/app/basket/basket.module.ts
+++ src/app/basket/basket.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BasketRoutingModule } from './basket-routing.module';
+import { BasketComponent } from './basket.component';
 
 @NgModule({
-  declarations: [  ],
+  declarations: [
+    BasketComponent
+   ],
   imports: [
     CommonModule,
     BasketRoutingModule
