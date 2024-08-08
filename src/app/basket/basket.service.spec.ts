1020955e156bab649aea9dea51b71e375066e2e1
Wilson Alves da Silva <wlsn.ads@gmail.com>
2024-08-08 10:24:29 -0300

    149.Adding an item to the basket service method

--- src/app/basket/basket.service.spec.ts
+++ src/app/basket/basket.service.spec.ts
@@ -1,16 +1,50 @@
-import { TestBed } from '@angular/core/testing';
 
+import { environment } from './../../environments/environment';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { BasketService } from './basket.service';
 
 describe('BasketService', () => {
   let service: BasketService;
+  let httpTestingController: HttpTestingController;
+  let baseUrl = environment.apiUrl;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [BasketService]
+    });
     service = TestBed.inject(BasketService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should retrieve basket from API via GET', () => {
+    const mockBasketId = '123';
+    const mockBasket = { id: '123', items: [] };
+
+    // Espiar o método `next` do BehaviorSubject
+    const spy = spyOn(service['basketSource'], 'next');
+
+    // Chamar o método que está sendo testado
+    service.getBasket(mockBasketId);
+
+    // Esperar a requisição HTTP e simular uma resposta
+    const req = httpTestingController.expectOne(`${service.baseUrl}basket?id=${mockBasketId}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockBasket);
+
+    // Verificar se o método `next` foi chamado com o valor correto
+    expect(spy).toHaveBeenCalledWith(mockBasket);
+  });
+
+
 });
--- src/app/basket/basket.service.ts
+++ src/app/basket/basket.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Basket } from '../shared/models/basket';
-import { BehaviorSubject } from 'rxjs';
+import { Basket, BasketItem } from '../shared/models/basket';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { Product } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -19,16 +20,61 @@ export class BasketService {
       error: (error) => console.log(error)
     })
   }
-
+  // getBasket(id: string): Observable<Basket> {
+  //   return this.http.get<Basket>(this.baseUrl + 'basket?id=' + id).pipe(
+  //     tap({
+  //       next: (response) => this.basketSource.next(response),
+  //       error: (error) => console.log(error)
+  //     })
+  //   );
+  // }
   setBasket(basket: Basket) {
     return this.http.post<Basket>(this.baseUrl + 'basket', basket).subscribe({
       next: basket => this.basketSource.next(basket),
       error: error => console.log(error)
     })
-   }
+  }
 
 
   getCurrentBasketValue() {
     return this.basketSource.value;
-   }
+  }
+
+  addItemToBasket(item: Product, quantity = 1) {
+    const itemToAdd: BasketItem = this.mapProductItemToBasketItem(item, quantity);
+    const basket = this.getCurrentBasketValue() ?? this.createBasket();
+    basket.items = this.AddOrUpdateItem(basket.items, itemToAdd, quantity);
+    this.setBasket(basket);
+
+  }
+
+  AddOrUpdateItem(items: BasketItem[], itemToAdd: BasketItem, quantity: number): BasketItem[] {
+    const item = items.find(i => i.id === itemToAdd.id);
+    if (item) {
+      item.quantity += quantity;
+    }
+    else {
+      itemToAdd.quantity = quantity;
+      items.push(itemToAdd);
+    }
+    return items;
+
+  }
+
+  createBasket(): Basket {
+    const basket = new Basket();
+    localStorage.setItem('basket_id', basket.id);
+    return basket;
+  }
+
+  private mapProductItemToBasketItem(item: Product, quantity: number): BasketItem {
+    return {
+      id: item.id,
+      productName: item.name,
+      price: item.price,
+      quantity: 0,
+      pictureUrl: item.pictureUrl,
+      type: item.productType
+    }
+  }
 }
